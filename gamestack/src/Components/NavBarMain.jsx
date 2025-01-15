ce0fe757f17a48c845bbcb2de9613b71fd70305a
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-01-15 16:01:54 +0600

    .

--- gamestack/src/Components/NavBarMain.jsx
+++ gamestack/src/Components/NavBarMain.jsx
@@ -5,10 +5,10 @@ export default function NavBarMain(){
     return(
         <>
             <div className="top-0 flex flex-row w-full justify-between h-16 items-center text-xl">
-                <Link className="ml-4"to="/home">
-                    <span className="text-yellow-500">Game</span>
-                    <span className="text-cyan-500">Stack</span>
-                </Link>
+                <div className="ml-4">
+                        <span className="text-yellow-500">Game</span>
+                        <span className="text-cyan-500">Stack</span>
+                </div>
                 <Link to="/profile">
                     <span className="text-green-500">Profile</span>
                 </Link>
