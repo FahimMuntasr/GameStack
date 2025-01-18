496492880369a32d925d3677e437feab134a1629
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-01-18 22:44:14 +0600

    Games filter added, non-functional

--- gamestack/src/Components/NavBarMain.jsx
+++ gamestack/src/Components/NavBarMain.jsx
@@ -4,16 +4,16 @@ import TextInput from "./TextInput";
 export default function NavBarMain(){
     return(
         <>
-            <div className="top-0 flex flex-row w-full justify-between h-16 items-center text-xl">
+            <div className="top-0 flex flex-row w-full justify-between h-16 items-center text-xl bg-zinc-950">
                 <div className="ml-4">
                         <span className="text-yellow-500">Game</span>
                         <span className="text-cyan-500">Stack</span>
                 </div>
                 <Link to="/profile">
-                    <span className="text-green-500">Profile</span>
+                    <span className="text-green-500 hover:text-green-300 transition-colors duration-100 ease-in">Profile</span>
                 </Link>
                 <Link to="/games">
-                    <span className="text-green-500">Games</span>
+                    <span className="text-green-500 hover:text-green-300 transition-colors duration-100 ease-in">Games</span>
                 </Link>
                 <TextInput text={"Search"} className={"mr-4"}/>
             </div>
--- gamestack/src/Pages/Games.jsx
+++ gamestack/src/Pages/Games.jsx
@@ -30,18 +30,81 @@ export default function Games(){
         fetchGames();
     }, []);
 
-    if (loading) return <p>Loading...</p>;
+    if (loading) return (
+        <div className="h-full flex justify-center items-center mt-96">
+            <p className=" text-violet-100 text-8xl">Loading...</p>
+        </div>
+    );
     if (error) return <p>Error: {error}</p>;
 
     return(
         <>
             <NavBarMain/>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-                  {games.map((game)=>(
-                    <Link to={'/profile'}>
-                        <GameCard key={game.id}game={game}/>
-                    </Link>
-                  ))}
+            <div className="flex flex-row">
+                <div className="flex flex-col bg-zinc-950 w-1/6">
+                    <div className="flex flex-col justify-center items-start p-5">
+                        <h1 className="font-bold text-stone-100 text-4xl pb-8">Browse</h1>
+                        <Link to={'/games'}className="text-stone-100 text-2xl">All Games</Link>
+                        <div>
+                            <span className="text-stone-100 text-2xl">Stores</span>
+                            <ul className="text-stone-100">
+                                <li>Steam</li>
+                                <li>PlayStation Store</li>
+                                <li>Xbox Store</li>
+                                <li>Nintendo Store</li>
+                                <li>GOG</li>
+                                <li>IOS App Store</li>
+                                <li>Google Play Store</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <span className="text-stone-100 text-2xl">Platforms</span>
+                            <ul className="text-stone-100">
+                                <li>PC</li>
+                                <li>PS5</li>
+                                <li>PS4</li>
+                                <li>Xbox X/S</li>
+                                <li>Xbox One</li>
+                                <li>Nintendo Switch</li>
+                                <li>IOS</li>
+                                <li>Android</li>
+                                <li>MacOS</li>
+                                <li>Linux</li>
+                                <li>Xbox 360</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <span className="text-stone-100 text-2xl">Genres</span>
+                            <ul className="text-stone-100">
+                                <li>Action</li>
+                                <li>Indie</li>
+                                <li>Adventure</li>
+                                <li>RPG</li>
+                                <li>Racing</li>
+                                <li>Sports</li>
+                                <li>Strategy</li>
+                                <li>Shooter</li>
+                                <li>Simulation</li>
+                                <li>Puzzle</li>
+                                <li>Arcade</li>
+                                <li>Platformer</li>
+                                <li>MMO</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className=" w-5/6 mr-8 mb-8 ml-8">
+                    <div className="flex justify-center">
+                        <h1 className="font-bold text-stone-100 text-6xl pt-7 pb-7">All Games</h1>
+                    </div>
+                    <div className="grid grid-cols-4 gap-8 ">
+                        {games.map((game)=>(
+                            <Link to={'/profile'} key={game.id}>
+                                <GameCard game={game}/>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             </div>
             <FootNote/>
         </>
