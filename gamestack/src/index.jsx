7e48bec1a7d8dbdcae3a59dd6ded4b3bc38810c5
Fahim Muntasir <byefahimmuntasir@gmail.com>
2025-05-04 15:13:33 +0600

    .

--- gamestack/src/index.jsx
+++ gamestack/src/index.jsx
@@ -1,8 +1,6 @@
-import {createRoot} from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-const root = createRoot(document.getElementById("root"))
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+const root = createRoot(document.getElementById("root"));
 
-root.render(
-    <App/>
-)
+root.render(<App />);
